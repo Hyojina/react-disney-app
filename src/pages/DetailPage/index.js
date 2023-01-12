b682cedc61205a0a32d4bf77035b6accac27f14e
Hyojina <hjkim3009@gmail.com>
2023-01-12 23:14:27 +0900

    feat: 영화 상세페이지 구현

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "../../api/axios";
 
 const DetailPage = () => {
-  return <div>DetailPage</div>;
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState({
+    backdrop_path: "/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg",
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`movie/${movieId}`);
+      setMovie(response.data);
+    }
+    fetchData();
+    console.log(movie);
+  }, [movieId]);
+
+  return (
+    <section>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt="poster"
+      />
+    </section>
+  );
 };
 
 export default DetailPage;
